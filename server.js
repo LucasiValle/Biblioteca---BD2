13f15ba743f62bc167d80966aefe36c1e51e7ee7
Miguel Paniago Flamini <paniagomigue@gmail.com>
2026-04-12 14:24:28 -0300

    Integração com MongoDB, rotas GET e POST de livros 'find e insert'

--- server.js
+++ server.js
@@ -1,25 +1,73 @@
+// Importa o framework Express (usado para criar o servidor web)
+const express = require("express");
+
+// Importa o cliente do MongoDB para conectar ao banco de dados
 const { MongoClient } = require("mongodb");
 
-// COLE A SUA STRING DO MONGODB AQUI
+// Cria a aplicação Express (nosso servidor)
+const app = express();
+
+// Permite que o servidor receba dados em formato JSON
+app.use(express.json());
+
+// String de conexão com o MongoDB local
 const uri = "mongodb://127.0.0.1:27017";
 
+// Cria o cliente MongoDB
 const client = new MongoClient(uri);
 
-async function run() {
-  try {
-    await client.connect();
+// Variável que vai armazenar a coleção "livros"
+let collection;
 
-    const db = client.db("biblioteca"); // troca aqui
-    const collection = db.collection("livros");
+// Função assíncrona para conectar ao banco de dados
+async function conectar() {
+  // Abre a conexão com o MongoDB
+  await client.connect();
 
-    const livros = await collection.find().toArray();
-    console.log(livros);
+  // Seleciona o banco de dados "biblioteca"
+  const db = client.db("biblioteca");
 
-  } catch (erro) {
-    console.log("Erro:", erro);
-  } finally {
-    await client.close();
-  }
+  // Seleciona a coleção "livros"
+  collection = db.collection("livros");
+
+  console.log("MongoDB conectado");
 }
 
-run();
+// Executa a conexão com o banco
+conectar();
+
+// Rota principal (teste)
+// Quando acessar http://localhost:3000
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando com MongoDB!");
+});
+
+app.get("/livros", async (req, res) => {
+  try {
+    const livros = await collection.find().toArray();
+    res.json(livros);
+  } catch (erro) {
+    console.log("Erro ao buscar livros:", erro);
+    res.status(500).send("Erro ao buscar livros");
+  }
+});
+
+// Inserir novo livro no banco de dados (insert)
+app.post ("/livros", async (req, res) => {
+  try {
+    //Insere os dados recebidos no corpo da requisição
+    await collection.insertOne(req.body);
+
+    res.send("Livro inserido com sucesso!");
+
+  }
+  catch (erro) {
+    console.log ("Erro ao inserir livro:", erro);
+    res.status(500).send("Erro ao inserir livro");
+  }
+});
+
+// Inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
